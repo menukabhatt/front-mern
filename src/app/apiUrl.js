38a29afc03979e77f467f77f23a66a769adf3c6c
menukabhatt <menukabhatt73@gmail.com>
2025-01-05 14:26:25 +0545

    xnvjkdnfb

--- src/app/apiUrl.js
+++ src/app/apiUrl.js
@@ -1,2 +1,8 @@
-export const baseUrl = 'http://192.168.1.75:5000/api';
-export const base = 'http://192.168.1.75:5000';
+export const baseUrl = 'https://backs-mern.onrender.com/api';
+export const base = 'https://backs-mern.onrender.com';
+
+
+
+
+// export const baseUrl = 'http://192.168.1.75:5000/api';
+// export const base = 'http://192.168.1.75:5000';
